emiride/lace

fix: replace `visible` prop with `open` on modals, tooltips and drawers (#385)

packages/staking/src/features/drawer/StakePoolDetailsDrawer.tsx
@@ -108,7 +108,7 @@ export const StakePoolDetailsDrawer = ({
 
   return (
     <Drawer
-      visible={isDrawerVisible}
+      open={isDrawerVisible}
       destroyOnClose
       onClose={closeDrawer}
       navigation={

BunsDev/lace-wallet

packages/staking/src/features/drawer/StakePoolDetailsDrawer.tsx
@@ -86,7 +86,7 @@ export const StakePoolDetailsDrawer = ({
 
   return (
     <Drawer
-      visible={drawerVisible}
+      open={drawerVisible}
       destroyOnClose
       onClose={closeDrawer}
       navigation={