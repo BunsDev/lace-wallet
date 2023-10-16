3a3d74a9a24965db3372f5f393c78584e53a5d8d
Lucas Araujo <lucas.araujo@iohk.io>
2023-10-16 14:05:46 -0300

    refactor: certificate inspector factory

--- apps/browser-extension-wallet/src/features/dapp/components/confirm-transaction/ConfirmDRepRetirementContainer.tsx
+++ apps/browser-extension-wallet/src/features/dapp/components/confirm-transaction/ConfirmDRepRetirementContainer.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { ConfirmDRepRetirement } from '@lace/core';
 import { SignTxData } from './types';
-import { dRepRetirementInspector, drepIDasBech32FromHash } from './utils';
+import { certificateInspectorFactory, drepIDasBech32FromHash } from './utils';
 import { Wallet } from '@lace/cardano';
 import { useWalletStore } from '@src/stores';
+
+const { CertificateType } = Wallet.Cardano;
 
 interface Props {
   signTxData: SignTxData;
@@ -13,11 +15,12 @@ interface Props {
 
 export const ConfirmDRepRetirementContainer = ({ signTxData, errorMessage }: Props): React.ReactElement => {
   const { t } = useTranslation();
-  const certificate = dRepRetirementInspector(signTxData.tx);
   const {
     walletUI: { cardanoCoin }
   } = useWalletStore();
-
+  const certificate = certificateInspectorFactory<Wallet.Cardano.UnRegisterDelegateRepresentativeCertificate>(
+    CertificateType.UnregisterDelegateRepresentative
+  )(signTxData.tx);
   const depositPaidWithCardanoSymbol = `${Wallet.util.lovelacesToAdaString(certificate.deposit.toString())} ${
     cardanoCoin.symbol
   }`;
